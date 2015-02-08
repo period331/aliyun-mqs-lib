c481a22ebe9c979616ded9b62fd159f8c6951844
baocaixiong <baocaixiong@gmail.com>
2015-02-08 15:17:30 +0800

    delete test aliyun key

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,13 @@ $loader->addPsr4('Mqs\\', __DIR__.'/Mqs');
 
 
 
-define('TEST_MQS_URL', 'iftbe2qyyp.mqs-cn-beijing.aliyuncs.com');
-define('TEST_MQS_ACCESS_KEY', 'LiUoB1wjqin5zf5t');
-define('TEST_MQS_ACCESS_SECRET', 'G0awx4iXrfPD4QeN7VOoM93JDbs17C');
+define('TEST_MQS_URL', '');
+define('TEST_MQS_ACCESS_KEY', '');
+define('TEST_MQS_ACCESS_SECRET', '');
+
+if (! TEST_MQS_ACCESS_KEY || ! TEST_MQS_ACCESS_SECRET || !TEST_MQS_URL) {
+    die('please set aliyun mqs key.');
+}
 
 \Mqs\Account::init(TEST_MQS_URL, TEST_MQS_ACCESS_KEY, TEST_MQS_ACCESS_SECRET);
 
